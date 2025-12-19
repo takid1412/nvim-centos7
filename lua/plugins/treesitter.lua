cd2fbeb88152a7f194deb500751af07c25bc7e11
takid1412 <duanpq92@gmail.com>
2025-12-19 18:48:51 +0700

    change opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "c", "python", "bash" },
-  },
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+        opts.ensure_installed = { "lua", "c", "python", "bash", "json" }
+    end,
 }
