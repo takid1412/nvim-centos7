f934dbf39840a10ed63f04e459b61508a5ce2d85
takid1412 <duanpq92@gmail.com>
2025-12-22 11:19:06 +0700

    change injection script for nginx

--- queries/nginx/injections.scm
+++ queries/nginx/injections.scm
@@ -1,5 +1,9 @@
 ;; extends
 
 ; Inject Lua into standard lua_blocks (content_by_lua_block, access_by_lua_block, etc.)
-(lua_block) @injection.content
-(#set! injection.language "lua")
+
+(directive
+  (directive_name) @name
+  (block) @injection.content
+  (#match? @name ".*_by_lua_block$")
+  (#set! injection.language "lua"))
