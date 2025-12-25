b9a0dfa28325333a715640d9e9da595c70537af1
takid1412 <takid1412@users.noreply.github.com>
2025-12-25 18:34:21 +0700

    Replace nvim-treesitter-openresty with nvim-treesitter-nginx

--- lua/plugins/language-syntax.lua
+++ lua/plugins/language-syntax.lua
@@ -1,7 +1,7 @@
 return {
     { "vim-scripts/iptables" },
     {
-        "takid1412/nvim-treesitter-openresty",
+        "takid1412/nvim-treesitter-nginx",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
         },
